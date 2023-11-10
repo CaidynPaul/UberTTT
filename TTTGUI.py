2b95ad071a20a85a23731261ae307d7864e42d42
Cyberfox <92183570+CaidynPaul@users.noreply.github.com>
2023-11-10 10:46:48 +0000

    Add GUI CODE

---
+++ TTTGUI.py
@@ -0,0 +1,65 @@
+import pygame
+
+pygame.init()
+def gameScreen():
+	SCREEN = pygame.display.set_mode((800,800))
+	SCREEN_RECT  = SCREEN.get_rect()
+
+	iX = pygame.image.load("X.png")
+	iO = pygame.image.load("O.png")
+
+	iX = pygame.transform.scale(iX,(64,64))
+	iO = pygame.transform.scale(iO,(64,64))
+
+	iXRect = iX.get_rect()
+	iORect = iO.get_rect()
+
+	board = [['o','x','o'],['x','x','o'],['o','o','o']]
+
+	def display_board(board):
+		for i in range(len(board)):
+			for q in range(len(board[i])):
+				if board[i][q] == "x":
+					SCREEN.blit(iX,(iXRect.x+64*q,iXRect.y+64*i))
+				if board[i][q] == "o":
+					SCREEN.blit(iO,(iORect.x+64*q,iORect.y+64*i))
+				if board[i][q] == " ":
+					pass
+
+	running = True
+
+	while running:
+		SCREEN.fill(('#e5e5e5'))
+		display_board(board)
+		for e in pygame.event.get():
+			if e.type == pygame.QUIT:
+				running = False
+				
+		pygame.display.flip()
+
+def titleScreen():
+	SCREEN = pygame.display.set_mode((1280,720))
+	SCREEN_RECT = SCREEN.get_rect()
+	
+	running = True
+	
+	play_font = pygame.font.SysFont("Arial",64)
+	play_text = play_font.render("Play",True,('#ffffff'))
+	
+	while running:
+		for e in pygame.event.get():
+			if e.type == pygame.QUIT:
+				running = False
+			
+			if e.type == pygame.MOUSEBUTTONDOWN:
+				mx,my = pygame.mouse.get_pos()
+				if mx >= SCREEN_RECT.centerx-(256//2) and mx <= SCREEN_RECT.centerx-(256//2)+256:
+					if my >= SCREEN_RECT.centery-128 and my <= SCREEN_RECT.centery-128+80:
+						gameScreen()
+						exit()
+				
+		pygame.draw.rect(SCREEN,('#6e6e6e'),[SCREEN_RECT.centerx-(256//2),SCREEN_RECT.centery-128,256,80])
+		SCREEN.blit(play_text,[SCREEN_RECT.centerx-50,SCREEN_RECT.centery-130])
+		pygame.display.flip()
+if __name__ == '__main__':
+	titleScreen()
